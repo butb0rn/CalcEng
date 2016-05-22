5edd03d278aa06b3a623db0d2e84a9f3669b063d
butb0rn <xxesnxx@gmail.com>
2016-05-22 18:39:25 -0500

    Add MathEquation class

--- src/com/butb0rn/calceng/Main.java
+++ src/com/butb0rn/calceng/Main.java
@@ -3,34 +3,26 @@ package com.butb0rn.calceng;
 public class Main {
 
 	public static void main(String[] args) {
-		double[] leftvals = {100.0d, 25.0d, 225.0d, 11.0d};
-		double[] rightvals = {50.0d, 92.0d, 17.0d, 3.0d};
-		char[] opCodes = {'d','a','s','m'};
-		double[] results = new double[opCodes.length];
 		
-		for(int i = 0; i < opCodes.length; i++) {
-			switch(opCodes[i]) {
-			case 'a': 
-				results[i] = leftvals[i] + rightvals[i];
-				break;
-			case 's':
-				results[i] = leftvals[i] - rightvals[i];
-				break;
-			case 'd':
-				results[i] = rightvals[i] != 0.0d ? leftvals[i] / rightvals[i] : 0.0d;
-				break;
-			case 'm':
-				results[i] = leftvals[i] * rightvals[i];
-				break;
-			default:
-				System.out.println("Error - invalid opCode");
-				results[i] = 0.0d;
-				break;
-			}
-		}
-		for(double res: results) {
-			System.out.println(res);
-		}
+		MathEquation[] equations = new MathEquation[4];
+		equations[0] = create(100.0d, 50.0d, 'd');
+		equations[1] = create(25.0d, 92.0d, 'a');
+		equations[2] = create(225.0d, 17.0d, 's');
+		equations[3] = create(11.0d, 3.0d, 'm');
 		
+		for(MathEquation equation: equations) {
+			equation.execute();
+			System.out.print("result = ");
+			System.out.println(equation.result);
+		}
+	}
+	
+	public static MathEquation create(double leftVal, double rightVal, char opCode) {
+		MathEquation equation = new MathEquation();
+		equation.leftVal = leftVal;
+		equation.rightVal = rightVal;
+		equation.opCode = opCode;
+		
+		return equation;
 	}
 }
