786e991cbf5059a1210b85f35e847c7b4f34002c
butb0rn <xxesnxx@gmail.com>
2016-05-22 17:35:29 -0500

    Using array for multiple  operations

--- src/com/butb0rn/calceng/Main.java
+++ src/com/butb0rn/calceng/Main.java
@@ -3,9 +3,28 @@ package com.butb0rn.calceng;
 public class Main {
 
 	public static void main(String[] args) {
+		double[] leftvals = {100.0d, 25.0d, 225.0d, 11.0d};
+		double[] rightvals = {50.0d, 92.0d, 17.0d, 3.0d};
+		char[] opCodes = {'d','a','s','m'};
+		double[] results = new double[opCodes.length];
 		
+		for(int i = 0; i < opCodes.length; i++) {
+			if (opCodes[i] == 'a') 
+				results[i] = leftvals[i] + rightvals[i];
+			else if(opCodes[i] == 's')
+				results[i] = leftvals[i] - rightvals[i];
+			else if(opCodes[i] == 'd')
+				results[i] = rightvals[i] != 0.0d ? leftvals[i] / rightvals[i] : 0.0d;
+			else if(opCodes[i] == 'm')
+				results[i] = leftvals[i] * rightvals[i];
+			else {
+				System.out.println("Error - invalid opCode");
+				results[i] = 0.0d;
+			}
+		}
+		for(double res: results) {
+			System.out.println(res);
+		}
 		
-
 	}
-
 }
