86c5958c6743a16fbe4ab2b91fd80709fe799235
butb0rn <xxesnxx@gmail.com>
2016-05-22 20:14:37 -0500

    Add a Divider and Multiplier class

---
+++ src/com/butb0rn/calceng/Multiplier.java
@@ -0,0 +1,14 @@
+package com.butb0rn.calceng;
+
+public class Multiplier extends CalculateBase {
+	public Multiplier() {};
+	public Multiplier(double leftVal, double rightVal) {
+		super(leftVal, rightVal);
+	}
+	
+	@Override
+	public void calculate() {
+		double value = getLeftVal() * getRightVal();
+		setResult(value);
+	}
+}
