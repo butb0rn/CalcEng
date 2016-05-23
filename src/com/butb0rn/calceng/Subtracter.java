014c7778d63cee1f5ac6fafddecd125fe0b0db51
butb0rn <xxesnxx@gmail.com>
2016-05-22 20:09:56 -0500

    Add a subtracter class

---
+++ src/com/butb0rn/calceng/Subtracter.java
@@ -0,0 +1,16 @@
+package com.butb0rn.calceng;
+
+public class Subtracter extends CalculateBase{
+	
+	public Subtracter() {}
+	public Subtracter(double leftVal, double rightVal) {
+		super(leftVal, rightVal);
+	}
+
+	@Override
+	public void calculate() {
+		double value = getLeftVal() - getRightVal();
+		setResult(value);
+	}
+	
+}
