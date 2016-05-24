361cb00a1389638cfb6a6a72e14477aef4cd6345
butb0rn <xxesnxx@gmail.com>
2016-05-23 23:15:34 -0500

    Add calculateHelper

--- src/com/butb0rn/calceng/Main.java
+++ src/com/butb0rn/calceng/Main.java
@@ -2,8 +2,24 @@ package com.butb0rn.calceng;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
+		//useMathEquation();
+		String[] statements = {
+				"divide 100.0 50.0",
+				"add 25.0 92.0",
+				"subtract 225.0 17.0",
+				"multiply 11.0 3.0"
+		};
 		
+		CalculateHelper helper = new CalculateHelper();
+		for(String statement:statements) {
+			helper.process(statement);
+			System.out.println(helper);
+		}
+		
+	}
+	
+	static void useMathEquation() {
 		MathEquation[] equations = new MathEquation[4];
 		equations[0] = new MathEquation('d', 100.0d, 50.0d);
 		equations[1] = new MathEquation('a', 25.0d, 92.0d);
@@ -30,6 +46,23 @@ public class Main {
 		equationOverload.execute(leftInt, rightInt);
 		System.out.print("result=");
 		System.out.println(equationOverload.getResult());
-	}
+		
+		System.out.println();
+		System.out.println("Using Inheritance");
+		System.out.println();
+		
+		CalculateBase[] calculators = {
+				new Divider(100.0d, 50.0d),
+				new Adder(25.0d, 92.0d),
+				new Subtracter(225.0d, 17.0d),
+				new Multiplier(11.0d, 3.0d)
+		};
+		
+		for (CalculateBase calculator: calculators) {
+			calculator.calculate();
+			System.out.print("result= ");
+			System.out.println(calculator.getResult());
+		}
 
+	}
 }
---
+++ src/com/butb0rn/calceng/MathCommand.java
@@ -0,0 +1,8 @@
+package com.butb0rn.calceng;
+
+public enum MathCommand {
+	Add,
+	Subtract,
+	Multiply,
+	Divide
+}
